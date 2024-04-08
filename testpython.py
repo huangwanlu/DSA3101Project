b9a91e1f2338e641072895b38fcf275ab241b988
jaidkamdar <jaidkamdar@gmail.com>
2024-04-08 19:31:31 +0800

    commit

---
+++ testpython.py
@@ -0,0 +1,17 @@
+pip install scikit-learn
+
+from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("kaggledata.csv")
+
